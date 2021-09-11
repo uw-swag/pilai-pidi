cada7d4f6e7fb5eef262e2df29be409a9136660f
Kishanthan <kshanth2101@gmail.com>
2021-09-11 10:31:25 -0400

    Capture pointer info in profile

--- src/main/java/ca/uwaterloo/swag/models/SliceProfile.java
+++ src/main/java/ca/uwaterloo/swag/models/SliceProfile.java
@@ -10,28 +10,31 @@ public class SliceProfile {
     public final String varName;
     public final String typeName;
     public final String definedPosition;
+    public final boolean isPointer;
     public final List<SliceVariableAccess> usedPositions = new ArrayList<>();
     public final Set<NamePos> dependentVars = new HashSet<>();
     public final Set<CFunction> cfunctions = new HashSet<>();
     public final Node functionNode;
 
     public SliceProfile(String fileName, String functionName, String varName, String typeName,
-                        String definedPosition) {
+                        String definedPosition, boolean isPointer) {
         this.fileName = fileName;
         this.functionName = functionName;
         this.varName = varName;
         this.typeName = typeName;
         this.definedPosition = definedPosition;
+        this.isPointer = isPointer;
         this.functionNode = null;
     }
 
     public SliceProfile(String fileName, String functionName, String varName, String typeName,
-                        String definedPosition, Node functionNode) {
+                        String definedPosition, boolean isPointer, Node functionNode) {
         this.fileName = fileName;
         this.functionName = functionName;
         this.varName = varName;
         this.typeName = typeName;
         this.definedPosition = definedPosition;
+        this.isPointer = isPointer;
         this.functionNode = functionNode;
     }
 }
